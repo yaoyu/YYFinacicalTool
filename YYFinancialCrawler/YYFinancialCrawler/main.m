c66fd40153ead4b87a4f2d1761df4a7a9c7a23d4
yaoyu <yao_yu@126.com>
2014-03-02 15:39:50 +0800

    Create

---
+++ YYFinancialCrawler/YYFinancialCrawler/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  YYFinancialCrawler
+//
+//  Created by yao_yu on 14-3-2.
+//  Copyright (c) 2014年 yao_yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
